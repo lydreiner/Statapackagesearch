f422898fb8ac7534aac3075a10ecc80cf0b1c79a
Lars Vilhuber <lars@vilhuber.com>
2021-11-12 11:26:56 -0500

    Forgot to change one line in the script...

--- test/verify_test1.sh
+++ test/verify_test1.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-if [[ -f run.log ]]
+if [[ -f test1.log ]]
 then
-   tail -8 run.log > actual_output.txt
+   tail -8 test1.log > actual_output.txt
    diff actual_output.txt test/expected_output1.txt || (echo "Not the expected output"; exit 2)
 else
    echo "Something went wrong with running Stata"
