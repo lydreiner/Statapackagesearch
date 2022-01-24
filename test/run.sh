b7e6af92432f855f4d007cc3c867d56c78e0d066
Lars Vilhuber <lars@vilhuber.com>
2022-01-24 08:29:10 -0500

    Minor robustness edits

--- test/run.sh
+++ test/run.sh
@@ -1,12 +1,22 @@
 #!/bin/bash
 
 # Assumes stata-mp is in the path
-# This does not work on Github Actions
 
 stata-mp -b test/test1.do
-if [[ -f verify_test.sh ]]
+if [[ -f verify_test1.sh ]]
 then
    ./verify_test1.sh
 else
    test/verify_test1.sh
+fi
+
+
+# Testing for what happens when there are long wors
+
+stata-mp -b test/test2.do
+if [[ -f verify_test2.sh ]]
+then
+   ./verify_test2.sh
+else
+   test/verify_test2.sh
 fi
--- test/verify_test1.sh
+++ test/verify_test1.sh
@@ -3,7 +3,7 @@
 if [[ -f test1.log ]]
 then
    tail -8 test1.log | cut -b -17 > actual_output.txt
-   diff actual_output.txt test/expected_output1.txt || (echo "Not the expected output"; exit 2)
+   diff actual_output.txt test/expected_output1.txt || (echo "Not the expected output: Test 1-1"; exit 2)
 else
    echo "Something went wrong with running Stata"
    exit 2
